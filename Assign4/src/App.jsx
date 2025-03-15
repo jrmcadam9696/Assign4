fcb06df1ffb9b3b08a5efd27b09042880672628c
Jason <jm5392@drexel.edu>
2025-03-15 17:01:13 -0400

    whatever

--- Assign4/src/App.jsx
+++ Assign4/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import React, { useState } from "react";
+import "./App.css";
+import Playlist from "./Playlist";
+import Status from "./Status";
+import Controls from "./controls";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentTrack, setCurrentTrack] = useState(null);
+  const [tracks, setTracks] = useState([]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Audio Player</h1>
+      <Playlist
+        setTracks={setTracks}
+        setCurrentTrack={setCurrentTrack}
+        currentTrack={currentTrack} // Pass the current playing track
+      />
+      <Status currentTrack={currentTrack} />
+      <Controls tracks={tracks} setCurrentTrack={setCurrentTrack} />
+    </div>
+  );
 }
 
-export default App
+export default App;
